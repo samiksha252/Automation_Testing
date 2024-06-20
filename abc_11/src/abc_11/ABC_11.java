cf3d1dea9758d979330cd1b441b14a6e9d8e6139
samiksha252 <samikshaaswale02@gmail.com>
2024-06-20 23:19:05 +0530

    ABC_project

---
+++ abc_11/src/abc_11/ABC_11.java
@@ -0,0 +1,16 @@
+package abc_11;
+
+public class ABC_11 {
+	
+	public void add() {
+		System.out.println("add number");
+	}
+
+	public static void main(String[] args) {
+        ABC_11 ok = new ABC_11();
+        
+        ok.add();
+
+	}
+
+}
