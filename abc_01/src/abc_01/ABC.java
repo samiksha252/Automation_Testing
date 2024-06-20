fa04064fd5c3b4ab14bd745ffb8b4e6450f43593
samiksha252 <samikshaaswale02@gmail.com>
2024-06-20 22:54:38 +0530

    ABC_project

---
+++ abc_01/src/abc_01/ABC.java
@@ -0,0 +1,14 @@
+package abc_01;
+
+public class ABC {
+	public void add() {
+		System.out.println("add number");
+	}
+
+	public static void main(String[] args) {
+	 ABC ok = new ABC();
+	 ok.add();
+
+	}
+
+}
