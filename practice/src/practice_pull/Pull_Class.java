6d72705dc2036a2c5bfb6a88568e217f775686fa
samiksha252 <samikshaaswale02@gmail.com>
2024-06-20 22:34:26 +0530

    practice

---
+++ practice/src/practice_pull/Pull_Class.java
@@ -0,0 +1,19 @@
+package practice_pull;
+
+public class Pull_Class {
+	
+	public void login() {
+		
+		System.out.println("login functionality");
+	}
+
+	public static void main(String[] args) {
+		
+		Pull_Class P = new Pull_Class();
+		
+		P.login();
+		
+
+	}
+
+}
